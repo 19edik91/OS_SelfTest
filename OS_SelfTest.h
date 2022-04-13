b5b2c8e5c816d88a7c22463d29bbc12233b271ee
kraemere91 <1991eduardkraemer@googlemail.com>
2022-04-13 20:55:27 +0200

    WIF-20: Added define switches and header comments

--- OS_SelfTest.h
+++ OS_SelfTest.h
@@ -5,55 +5,29 @@
 
 \file       SelfTest.handler
 \brief      Module for Testroutines
-
+            Tests routines are executed at startup or during normal operation.
+*           The routines where implemented to fullfil the requirements of DIN 60730.
+*           There are two groups of tests: Tests that run once befor entry of main()
+*           Tests called cyclically during execution of main()- loop
 ***********************************************************************************/
 #ifndef SELFTEST_H
 #define SELFTEST_H
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
+
+#include "OS_Config.h"
+#ifdef USE_OS_SELF_TEST
+/********************************* includes **********************************/
 
 #include "BaseTypes.h"
-#include "OS_Config.h"
-
-
-#ifdef USE_SELFTEST
-/**************************************************************
-*
-* Tests routines are executed at startup or during normal operation.
-* The routines where implemented to fullfil the requirements of DIN 60730.
-* There are two groups of tests: Tests that run once befor entry of main()
-*                                Tests called cyclically during execution of main()- loop
-*
-***************************************************************/
-
-/***************************************
- * Type Defs for all Selftests
- ***************************************/
-
-typedef enum
-{
-   eSelfTest_OK,                        // STest ok
-   eSelfTest_ERROR,                     // STest Error
-   eSelfTest_NotExecuted                // STest Not executed
-} teSelfTest_ResultCode;
-
-
-/*******************************************
- * Status flags to return function results *
- *******************************************/
-
+/***************************** defines / macros ******************************/
+//Status flags to return function results
 #define OK_STATUS                   (0u)
 #define ERROR_STATUS                (1u)
 #define PASS_STILL_TESTING_STATUS   (2u)
 #define PASS_COMPLETE_STATUS        (3u)
-
-//******************************
-//* Defines for timing tests   *
-//******************************
-
+    
+    
+// Defines for timing tests 
 #if ( ST_TESTPIN_ACTIVE )
     #if ( CONTROL_LOW_VOLT )
         #define ST_TESTPIN_ON             Pin_6_0_Write( 1 );     // Switch on test pin
@@ -71,31 +45,35 @@ typedef enum
     #define ST_TESTPIN_ON                                     // Switch on test pin
     #define ST_TESTPIN_OFF                                    // Switch on test pin
 #endif
+    
+/****************************** type definitions *****************************/
+typedef enum
+{
+   eSelfTest_OK,                        // STest ok
+   eSelfTest_ERROR,                     // STest Error
+   eSelfTest_NotExecuted                // STest Not executed
+} teSelfTest_ResultCode;
 
-/***************************************
- * Variables
- ***************************************/
-
+/***************************** global variables ******************************/
 // Variables used in isr_1 test interrupt handler
 static volatile u32 uSfTtInterruptCnt;
 
-/***************************************
-* Function Prototypes
-***************************************/
-
-
+/************************ externally visible functions ***********************/
+#ifdef __cplusplus
+extern "C"
+{
+#endif
 
 void OS_SelfTest_InitCyclic(void);
 void OS_SelfTest_Cyclic_Run(void);
 void OS_SelfTest_StartCallback(void);
 
-#endif
-
 #ifdef __cplusplus
 }
 #endif
 
+#endif //USE_OS_SELF_TEST
+
 #endif // SELFTEST_H
 
-/* [] END OF FILE */
 
